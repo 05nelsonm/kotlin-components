f640e8853890f40ea5acf8e376be50b2a6911009
Matthew Nelson <developer@matthewnelson.io>
2021-06-30 06:18:57 -0400

    initialize project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # components
+
+A git repository for managing dependency versions across multiple projects via use of git submodules
---
+++ app/src/main/java/io/matthewnelson/components/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.matthewnelson.components
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity(R.layout.activity_main)
