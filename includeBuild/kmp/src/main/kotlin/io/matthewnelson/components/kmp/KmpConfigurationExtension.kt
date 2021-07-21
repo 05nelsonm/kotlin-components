19e3bd23515320068c91cba7b941e74f2ffeea48
Matthew Nelson <developer@matthewnelson.io>
2021-07-20 21:26:24 -0400

    refactor kmp-configure plugin using kotlin dsl

---
+++ includeBuild/kmp/src/main/kotlin/io/matthewnelson/components/kmp/KmpConfigurationExtension.kt
@@ -0,0 +1,185 @@
+/*
+ *  Copyright 2021 Matthew Nelson
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * */
+package io.matthewnelson.components.kmp
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.invoke
+import javax.inject.Inject
+
+open class KmpConfigurationExtension @Inject constructor(private val project: Project) {
+
+    companion object {
+        private val ANDROID_INSTANCE = KmpTarget.ANDROID("30.0.3", 30, 23, 30)
+        private val ALL_TARGETS: Set<KmpTarget> = setOf(
+            ANDROID_INSTANCE,
+
+            KmpTarget.IOS.ARM32,
+            KmpTarget.IOS.ARM64,
+            KmpTarget.IOS.X64,
+
+            KmpTarget.JS.IR,
+            KmpTarget.JS.LEGACY,
+
+            KmpTarget.JVM,
+
+            KmpTarget.LINUX.ARM32HFP,
+            KmpTarget.LINUX.MIPS32,
+            KmpTarget.LINUX.MIPSEL32,
+            KmpTarget.LINUX.X64,
+
+            KmpTarget.MACOS.X64,
+
+            KmpTarget.MINGW.X64,
+            KmpTarget.MINGW.X86,
+
+            KmpTarget.TVOS.ARM64,
+            KmpTarget.TVOS.X64,
+
+            KmpTarget.WATCHOS.ARM32,
+            KmpTarget.WATCHOS.ARM64,
+            KmpTarget.WATCHOS.X64,
+            KmpTarget.WATCHOS.X86,
+        )
+    }
+
+    @Suppress("unused")
+    fun setupMultiplatform(targets: List<KmpTarget>): Boolean {
+        if (targets.isEmpty()) {
+            return false
+        }
+
+        val enabledTargets: Set<KmpTarget> = getEnabledTargets(project)
+
+        var android: KmpTarget.ANDROID? = null
+        if (enabledTargets.contains(ANDROID_INSTANCE)) {
+            for (target in targets) {
+                if (target is KmpTarget.ANDROID) {
+                    android = target
+                    break
+                }
+            }
+
+            if (android != null) {
+                project.plugins.apply("com.android.library")
+            }
+        }
+
+        project.plugins.apply("org.jetbrains.kotlin.multiplatform")
+
+        setupMultiplatformCommon(project)
+
+        android?.setupMultiplatform(project)
+
+        for (target in targets) {
+            if (target !is KmpTarget.ANDROID && target !is KmpTarget.JS && enabledTargets.contains(target)) {
+                target.setupMultiplatform(project)
+            }
+        }
+
+        if (enabledTargets.contains(KmpTarget.JS.IR) || enabledTargets.contains(KmpTarget.JS.LEGACY)) {
+            if (targets.contains(KmpTarget.JS.IR) || targets.contains(KmpTarget.JS.LEGACY)) {
+                setupMultiplatformJs(
+                    project = project,
+                    isIr = targets.contains(KmpTarget.JS.IR),
+                    isLegacy = targets.contains(KmpTarget.JS.LEGACY),
+                )
+            }
+        }
+
+        return true
+    }
+
+    private fun setupMultiplatformCommon(project: Project) {
+        project.kotlin {
+            sourceSets {
+
+                getByName(KmpTarget.COMMON_MAIN) {
+
+                }
+
+                getByName(KmpTarget.COMMON_TEST) {
+                    dependencies {
+                        implementation(kotlin("test-common"))
+                        implementation(kotlin("test-annotations-common"))
+                    }
+                }
+
+                // TODO: Setup default platform specific structures
+//                    maybeCreate("exampleMain").dependsOn(getByName(COMMON_MAIN))
+//                    maybeCreate("exampleTest").dependsOn(getByName(COMMON_TEST))
+            }
+        }
+    }
+
+    private fun setupMultiplatformJs(project: Project, isIr: Boolean, isLegacy: Boolean) {
+        project.kotlin {
+            js(
+                if (isIr && isLegacy) {
+                    BOTH
+                } else if (isIr && !isLegacy) {
+                    IR
+                } else {
+                    LEGACY
+                }
+            ) {
+                browser()
+                nodejs()
+            }
+
+            sourceSets {
+                // Both JS_IR and JS_LEGACY main/test names are the same
+                maybeCreate(KmpTarget.JS.IR.sourceSetMainName)
+                maybeCreate(KmpTarget.JS.IR.sourceSetTestName).dependencies {
+                    implementation(kotlin("test-js"))
+                }
+            }
+        }
+    }
+
+    private fun getEnabledTargets(project: Project): Set<KmpTarget> {
+        val propertyTargets: Any? = project.findProperty("KMP_TARGETS")
+
+        return if (propertyTargets != null && propertyTargets is String && propertyTargets.isNotEmpty()) {
+
+            val map: Map<String, KmpTarget> = ALL_TARGETS.associateBy { it.getCommandLinePropertyValue() }
+
+            val set: Set<KmpTarget> = propertyTargets
+                .split(",")
+                .mapNotNull { propertyTarget ->
+                    map[propertyTarget].let { target ->
+                        if (target == null) {
+                            println("\nWARNING: KMP_TARGET property value '$propertyTarget' not recognized...")
+                        }
+
+                        target
+                    }
+                }
+                .toSet()
+
+            if (set.isEmpty()) {
+                throw IllegalArgumentException("KMP_TARGETS property is set but did not contain any matching values")
+            }
+
+            set
+        } else {
+            println(
+                "\nWARNING: KMP_TARGETS property not found..." +
+                        "\n         Enabling all targets for project '" + project.name + "'\n"
+            )
+            ALL_TARGETS
+        }
+    }
+}
---
+++ includeBuild/kmp/src/main/kotlin/io/matthewnelson/components/kmp/KmpConfigurationPlugin.kt
@@ -0,0 +1,68 @@
+/*
+ *  Copyright 2021 Matthew Nelson
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * */
+@file:Suppress("SpellCheckingInspection")
+
+package io.matthewnelson.components.kmp
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Add to module's build.gradle.kts file:
+ *
+ * plugins {
+ *     id("kmp-configuration")
+ * }
+ *
+ * kmpConfiguration {
+ *     setupMultiplatform(
+ *         listOf(
+ *             // list all kmp targets that the module will utilize
+ *             KmpTarget.ANDROID(
+ *                 "30.0.3",                                // buildToolsVersion
+ *                 30,                                      // compileSdk
+ *                 16,                                      // minSdk
+ *                 30,                                      // targetSdk
+ *                 "src/androidMain/AndroidManifest.xml"    // OPTIONAL arg to specify manifest path
+ *             ),
+ *             KmpTarget.IOS.ARM32,
+ *             KmpTarget.IOS.ARM64,
+ *             KmpTarget.IOS.X64,
+ *             ...
+ *         )
+ *     )
+ * }
+ *
+ * Append args when building from command line for the given machine (linux, macos, windows):
+ *  - ex: `$ ./gradlew build -PKMP_TARGETS=ANDROID,JS_IR,JS_LEGACY,JVM,LINUX_X64,...`
+ *
+ * Full list of KMP_TARGET property arguments:
+ *
+ *   ANDROID,JS_IR,JS_LEGACY,JVM,LINUX_ARM32HFP,LINUX_MIPS32,LINUX_MIPSEL32,
+ *   LINUX_X64,IOS_ARM32,IOS_ARM64,IOS_X64,MACOS_X64,MINGW_X64,MINGW_X86,
+ *   TVOS_ARM64,TVOS_X64,WATCHOS_ARM32,WATCHOS_ARM64,WATCHOS_X64,WATCHOS_X86
+ *
+ * Shout out to Arkivanov for his work on Reaktive, Decompose and MVIKotlin which heavily influenced this
+ *  - https://github.com/badoo/Reaktive
+ *  - https://github.com/arkivanov/Decompose
+ *  - https://github.com/arkivanov/MVIKotlin
+ * */
+@Suppress("unused")
+class KmpConfigurationPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        target.extensions.create("kmpConfiguration", KmpConfigurationExtension::class.java, target)
+    }
+}
