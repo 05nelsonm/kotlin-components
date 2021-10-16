feab78f8bd6bc14f1be82a7a0669f3ec8e1bbab5
Matthew Nelson <developer@matthewnelson.io>
2021-10-16 17:05:43 -0400

    remove dependencies plugin in favor of importing the jar files

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,26 +1,11 @@
-@file:Suppress("SpellCheckingInspection")
-
-import io.matthewnelson.kotlin.components.dependencies.plugins as Plugins
-
 plugins {
     `kotlin-dsl`
-    id("dependencies")
 }
 
 repositories {
-    mavenCentral()
-    google()
     gradlePluginPortal()
 }
 
-System.setProperty("GRADLE_ANDROID", Plugins.android.gradle)
-System.setProperty("GRADLE_ANDROIDX_NAVIGATION_SAFEARGS", Plugins.androidx.navigation.safeArgs)
-System.setProperty("GRADLE_GOOGLE_HILT", Plugins.google.hilt)
-System.setProperty("GRADLE_VERSIONS", Plugins.gradleVersions)
-System.setProperty("GRADLE_INTELLIJ", Plugins.intellijGradle)
-System.setProperty("GRADLE_KOTLIN_DOKKA", Plugins.kotlin.dokka)
-System.setProperty("GRADLE_KOTLIN", Plugins.kotlin.gradle)
-System.setProperty("GRADLE_SERIALIZATION", Plugins.kotlin.serialization)
-System.setProperty("GRADLE_MAVEN_PUBLISH", Plugins.mavenPublish)
-System.setProperty("GRADLE_SQUARE_EXHAUSTIVE", Plugins.square.exhaustive)
-System.setProperty("GRADLE_SQUARE_SQLDELIGHT", Plugins.square.sqlDelight)
+dependencies {
+    implementation(files("../includeBuild/dependencies/build/libs/dependencies-SNAPSHOT.jar"))
+}
