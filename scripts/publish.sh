9da106ad28df93a750280b0d2b818553a83050c0
Matthew Nelson <developer@matthewnelson.io>
2022-05-05 20:15:15 -0400

    Add individual publishing tasks (#59)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,14 +5,24 @@ WORK_DIR=$(pwd)
 function help() {
   echo "publish.sh help"
   echo ""
-  echo "publish.sh <command> <option>"
+  echo "publish.sh <command> <option> <flag>"
   echo ""
-  echo "            --host                 builds and publishes gradle project for specified platform"
-  echo "                       linux"
+  echo "            --host                        Builds and publishes gradle project for specified platform"
+  echo ""
+  echo "                       linux  --non-jvm   If --non-jvm flag present, will build and publish only the"
+  echo "                                          Js, Linux, and Mingw targets. If no flag present, will"
+  echo "                                          publish everything (ie. publishAllPublicationsToMavenCentralRepository)"
+  echo ""
   echo "                       darwin"
   echo "                       mingw"
   echo ""
-  echo "            --check-publication    verifies MavenCentral publications are good"
+  echo "            --check-publication           Verifies MavenCentral publications are good"
+  echo ""
+  echo "            --publish-android             Publishes only the android source set"
+  echo ""
+  echo "            --publish-jvm                 Publishes only the jvm source set"
+  echo ""
+  echo "            --publish-multiplatform       Publishes the multiplatform sources"
   echo ""
 }
 
@@ -44,26 +54,62 @@ function checkCheckPublicationProjectExists() {
 }
 
 function clean() {
-  ./gradlew clean
+  echo ""
+  echo "    SCRIPT ./gradlew clean -DKMP_TARGETS_ALL"
+  echo ""
+
+  ./gradlew clean -DKMP_TARGETS_ALL
 }
 
 function sync() {
-  ./gradlew prepareKotlinBuildScriptModel $1
+  # $1 = KMP_TARGETS
+
+  echo ""
+  echo "    SCRIPT: ./gradlew prepareKotlinBuildScriptModel $1"
+  echo ""
+
+  if ! ./gradlew prepareKotlinBuildScriptModel $1; then
+    echo ""
+    echo "    SCRIPT: Sync failure. Exiting"
+    echo ""
+    exit 1
+  fi
 }
 
 function build() {
-  ./gradlew build --no-daemon --no-parallel $1
+  # $1 = KMP_TARGETS
+
+  echo ""
+  echo "    SCRIPT: ./gradlew build --no-daemon --no-parallel $1"
+  echo ""
+
+  if ! ./gradlew build --no-daemon --no-parallel $1; then
+    echo ""
+    echo "    SCRIPT: Build failure. Exiting."
+    echo ""
+    exit 1
+  fi
 }
 
 function getPublishTasks() {
-  ./gradlew tasks |
+  # $1 = KMP_TARGETS
+  # $2 = grep filter
+
+  ./gradlew tasks $1 |
   grep "ToMavenCentralRepository" |
   cut -d ' ' -f 1 |
-  grep $1
+  grep $2
 }
 
 function publish() {
-  ./gradlew $1 --no-parallel --no-daemon $2
+  # $1 = publish tasks
+  # $2 = KMP_TARGETS
+
+  echo ""
+  echo "    SCRIPT: ./gradlew $1 --no-daemon --no-parallel $2"
+  echo ""
+
+  ./gradlew $1 --no-daemon --no-parallel $2
 }
 
 case $1 in
@@ -76,27 +122,58 @@ case $1 in
     case $2 in
 
       "linux")
-        build "-DKMP_TARGETS_ALL"
-        publish "publishAllPublicationsToMavenCentralRepository" "-DKMP_TARGETS_ALL"
+        case $3 in
+            "--non-jvm")
+              # JVM is needed here so JS build won't throw compilation errors
+              TARGETS="-PKMP_TARGETS=JVM,JS,LINUX_ARM32HFP,LINUX_MIPS32,LINUX_MIPSEL32,LINUX_X64,MINGW_X64,MINGW_X86"
+              sync "$TARGETS"
+              PUBLISH_TASKS=$(getPublishTasks "$TARGETS" '-e publishJs -e publishLinux -e publishMingw')
+
+              if [ "$PUBLISH_TASKS" != "" ]; then
+                build "$TARGETS"
+                publish "$PUBLISH_TASKS" "$TARGETS"
+              else
+                echo ""
+                echo "    SCRIPT: No non-jvm publication tasks available"
+                echo ""
+                exit 1
+              fi
+              ;;
+
+            *)
+              build "-DKMP_TARGETS_ALL"
+              publish "publishAllPublicationsToMavenCentralRepository" "-DKMP_TARGETS_ALL"
+              ;;
+        esac
         ;;
 
       "darwin")
-        sync
-        PUBLISH_TASKS=$(getPublishTasks '-e Ios -e Macos -e Tvos -e Watchos')
-        build
-        publish "$PUBLISH_TASKS"
+        TARGETS="-PKMP_TARGETS=IOS_ALL,IOS_ARM32,IOS_ARM64,IOS_X64,MACOS_ARM64,MACOS_X64,TVOS_ALL,TVOS_ARM64,TVOS_X64,WATCHOS_ALL,WATCHOS_ARM32,WATCHOS_ARM64,WATCHOS_X64,WATCHOS_X86,IOS_SIMULATOR_ARM64,TVOS_SIMULATOR_ARM64,WATCHOS_SIMULATOR_ARM64"
+        sync "$TARGETS"
+        PUBLISH_TASKS=$(getPublishTasks "$TARGETS" '-e publishIos -e publishMacos -e publishTvos -e publishWatchos')
+
+        if [ "$PUBLISH_TASKS" != "" ]; then
+          build "$TARGETS"
+          publish "$PUBLISH_TASKS" "$TARGETS"
+        else
+          echo ""
+          echo "    SCRIPT: No darwin publication tasks available"
+          echo ""
+          exit 1
+        fi
         ;;
 
       "mingw")
-        sync
-        PUBLISH_TASKS=$(getPublishTasks '-e Mingw')
-        build
+        TARGETS="-PKMP_TARGETS=MINGW_X64,MINGW_X86"
+        sync "$TARGETS"
+        PUBLISH_TASKS=$(getPublishTasks "$TARGETS" '-e Mingw')
+        build "$TARGETS"
 
         echo ""
         echo "Windows hates bash..."
         echo "paste the following into powershell"
         echo ""
-        echo "./gradlew $PUBLISH_TASKS --no-parallel --no-daemon"
+        echo "./gradlew $PUBLISH_TASKS --no-parallel --no-daemon $TARGETS"
         echo ""
         sleep 15
         ;;
@@ -116,6 +193,30 @@ case $1 in
     checkCheckPublicationProjectExists
     clean
     ./gradlew :tools:check-publication:build --refresh-dependencies -PCHECK_PUBLICATION
+    ;;
+
+  "--publish-android")
+    checkGradlewExists
+    checkKotlinComponentsProject
+    clean
+    build "-PKMP_TARGETS=ANDROID,JVM"
+    publish "publishAndroidReleasePublicationToMavenCentralRepository" "-PKMP_TARGETS=ANDROID,JVM"
+    ;;
+
+  "--publish-jvm")
+    checkGradlewExists
+    checkKotlinComponentsProject
+    clean
+    build "-PKMP_TARGETS=JVM"
+    publish "publishJvmPublicationToMavenCentralRepository" "-PKMP_TARGETS=JVM"
+    ;;
+
+  "--publish-multiplatform")
+    checkGradlewExists
+    checkKotlinComponentsProject
+    clean
+    build "-DKMP_TARGETS_ALL"
+    publish "publishKotlinMultiplatformPublicationToMavenCentralRepository" "-DKMP_TARGETS_ALL"
     ;;
 
   *)
